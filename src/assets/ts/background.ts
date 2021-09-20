768d981ffb3f6ce46db060393064c8534dc90189
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-09-20 11:13:59 +0600

    Update background.ts

--- src/assets/ts/background.ts
+++ src/assets/ts/background.ts
@@ -5,10 +5,47 @@
 * Official Link: https://github.com/mralaminahamed/capture-images
 * */
 //External dependencies
-import {runtime, storage, contextMenus, browserAction} from "webextension-polyfill";
-//Internal dependencies
-import {createCaptureImageContextMenu, handleUpdateAvailable, sendCaptureImagesRequest} from "./lib.background";
+import {runtime, storage, contextMenus, browserAction, tabs} from "webextension-polyfill";
 
+
+
+//working functions
+export function createCaptureImageContextMenu() {
+    //console.log(window.location)
+    contextMenus.create({
+        "id": "captureImagesContextMenu",
+        "title": "Capture Images",
+        "contexts": ["page"]
+    });
+}
+
+export function sendCaptureImagesRequest(e:any) {
+    console.log(e)
+    tabs.query({active: true, currentWindow: true}).then(function(tabs:any) {
+        //console.log(tabs);
+        tabs.sendMessage(tabs[0].id, {
+            session: 'makePopUp',
+            logo: runtime.getURL('assets/images/image.svg'),
+            title: runtime.getManifest().name,
+        });
+    });
+}
+
+
+
+//upgrade add-on on available
+export function handleUpdateAvailable(details:any) {
+    console.log(details);
+    //console.log(details.version);
+    // Proceed to upgrade the add-on
+    runtime.reload();
+}
+
+
+
+
+
+//action
 
 /*add context menu*/
 runtime.onInstalled.addListener(createCaptureImageContextMenu);
@@ -23,7 +60,4 @@ browserAction.onClicked.addListener(sendCaptureImagesRequest);
 
 //make update
 runtime.onUpdateAvailable.addListener(handleUpdateAvailable);
-
-
-
 
