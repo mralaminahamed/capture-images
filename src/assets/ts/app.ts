6318814772d49c2ee69ac6cfa2a283a12597a98e
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-01 10:31:30 +0600

    Update app.ts

--- src/assets/ts/app.ts
+++ src/assets/ts/app.ts
@@ -7,18 +7,21 @@
 
 
 import {runtime} from "webextension-polyfill";
-import {
-    assignAttributes,
-    captureElement
-} from "./lib";
+import {assignAttributes, captureElement, captureElementList, createElement} from "./lib";
 import {UI} from "./ui";
-import {crawlImages} from "./crawler";
+import {makeView} from "./crawler";
+import FileSaver from "file-saver";
 
+//let imageNodeList: any[];
+
+//create ui root fo ci application
+let app = createElement('div', {
+    'id': 'ci', 'class': 'ci-window', 'style': 'display:none !important;'
+});
+captureElement('body').appendChild(app);
 
 UI.make();
 
-//crawling images
-let imageNodeList: any[] = crawlImages();
 
 runtime.onMessage.addListener(
     (request, sender):any => {
@@ -45,23 +48,57 @@ runtime.onMessage.addListener(
                 //Footer
                 assignAttributes(captureElement('#ci-footer'), {class:'ci-footer', text:(new Date()).getFullYear() + '@ CI Author'});
 
-
-
                 //make display block
                 captureElement('#ci').style.display = 'block';
                 //captureElement('#ci').addEventListener('scrolled');
                 document.querySelector('#ci')?.addEventListener('scroll', function (e) {
                     captureElement('#ci-window').style.top = '70px';
-                })
+                });
 
-                if(document.images.length > 0){
-                    for (let j = 0; j < imageNodeList.length; j++) {
-                        document.querySelector('#ci-container')?.appendChild(imageNodeList[j])
-                    }
+
+                // select all images from current webpage
+                let imageNodeList: any[]  = captureElementList('img');
+
+                console.log('make ui for download')
+                console.log(imageNodeList)
+                console.log(imageNodeList.length)
+
+                let totalImages = new Set(imageNodeList);
+                console.log(totalImages)
+                console.log(totalImages.size)
+                if( totalImages.size > 0){
+                    totalImages.forEach(function (element) {
+
+                        if (element.id !== 'ci-logo'){
+                            //attach download icon for quick download
+                            //create div element for icon
+                            let buttonDiv = document.createElement('div');
+                            buttonDiv.setAttribute('style',"display: flex;justify-content: flex-end;width: 100%;position: absolute;background-color: red;z-index: 1000;")
+
+                            //create a element for icon setting
+                            let quickDownloadButton = document.createElement('a')
+                            quickDownloadButton.setAttribute('class','quick-download')
+                            // quickDownloadButton.setAttribute('style',"z-index: 1000;")
+
+                            quickDownloadButton.setAttribute('href',element.src)
+                            quickDownloadButton.addEventListener('click',function (event) {
+                                event.preventDefault();
+                                FileSaver.saveAs(this.href);
+                            })
+                            quickDownloadButton.innerHTML = '⮋';
+                            buttonDiv.appendChild(quickDownloadButton);
+                            element.parentElement.insertBefore(buttonDiv, element);
+                        }
+
+                        //attach to ci container for batch download
+                        let elementDOM = makeView(element);
+                        document.querySelector('#ci-container')?.appendChild(elementDOM)
+                    });
                 }
+
                 return Promise.resolve('downloaded');
             } else {
-                alert('Please  until page load complete.')
+                alert('Please until page load complete.')
             }
         }
     }
